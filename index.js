bc088b3945e97c55bcb3d190d4947f9b91afb0b0
Moin Khan <kmoin786@gmail.com>
2020-11-05 08:36:12 +0530

    configure express

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It worksSS!\n',
-        version = 'NodeJS ' + process.env.WEATHER_API_KEY + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
+const express = require('express');
+const app = express();
+
+app.listen();
+app.use(express.static('public'))
