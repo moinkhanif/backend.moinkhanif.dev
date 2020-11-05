51aa48fec2ea86c3b717848bec5fa8e11e91fc98
Moin Khan <kmoin786@gmail.com>
2020-11-05 10:19:01 +0530

    add test get

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const app = express();
 
 app.listen();
 app.use(express.static('public'))
+app.use(express.json({limit: '1mb'}));
+
+app.get('/gh-pinned', (req, res) => {
+  res.json({
+    "status": "ok"
+  })
+})
