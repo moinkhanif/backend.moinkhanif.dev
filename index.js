f0e7044a2da292bc7d54b660eaa6ba4937e9a666
Moin Khan <kmoin786@gmail.com>
2020-11-05 11:05:23 +0530

    remove node modules dir

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.listen();
+app.listen(3000);
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}));
 
 app.get('/gh-pinned', (req, res) => {
   res.json({
     "status": "ok",
-    "req": "hmm"
+    "req": req.body
   })
 })
