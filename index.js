1e29594309df1549a336572ab506c1f1a9e24b50
Moin Khan <kmoin786@gmail.com>
2020-11-06 06:27:27 +0530

    create weathery endpoint

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 app.listen(3000);
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}));
+
+app.post('/weathery', (req, res) => {
+  res.json({
+    'status': 'success'
+  })
+})
+
+app.get('/api/v1/gh-pinned', (req,res) => {
+  res.json({
+    "ok": "ok"
+  })
+})
 
 app.get('/gh-pinned', (req, res) => {
   res.json({
