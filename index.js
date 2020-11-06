bf26badb4e38e0b9ad333cff58fb412100841356
Moin Khan <kmoin786@gmail.com>
2020-11-06 07:28:11 +0530

    add weather logic to get data

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const fetch = require("node-fetch");
+
 
 app.listen(3000);
 app.use(express.static('public'))
 app.use(express.json({limit: '1mb'}));
 
-app.post('/api/v1/weathery', (req, res) => {
-  res.json({
-    'status': process.env.WEATHER_API_KEY
-  })
+const getweather = async ({place}) => {
+  const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${process.env.WEATHER_API_KEY}`)
+  const weatherJSON = await weather.json()
+  return weatherJSON
+}
+
+app.post('/api/v1/weathery', async (req, res) => {
+  const lot = await getweather(req.body)
+  res.json(lot)
 })
 
 app.get('/api/v1/gh-pinned', (req,res) => {
